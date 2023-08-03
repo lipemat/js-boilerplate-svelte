f5d100af458a5f756e3df90b8bb74c0cb9a3edaf
lipemat <mat@matlipe.com>
2023-08-03 17:31:54 -0500

    Add getTypeScriptConfig helper

---
+++ helpers/config.js
@@ -0,0 +1,17 @@
+const requireJSON5 = require( 'require-json5' );
+
+/**
+ * Get the TypeScript config used by Svelte preprocess.
+ *
+ * Using the config from @tsconfig/svelte/tsconfig.json verbaitim.
+ *
+ * @notice We must use require-json5 because the config uses comments.
+ */
+function getTypeScriptConfig() {
+	return requireJSON5( require.resolve( '@tsconfig/svelte/tsconfig.json' ) );
+}
+
+
+module.exports = {
+	getTypeScriptConfig,
+}
