4684f74ee27b078f8f179a3f24666066d1996c2e
lipemat <mat@matlipe.com>
2026-01-07 17:56:44 -0500

    Remove dangling snapshot resolver

--- jest/jest.config.ts
+++ jest/jest.config.ts
@@ -2,9 +2,6 @@ import type {Config} from 'jest';
 
 const config: Config = require( '@lipemat/js-boilerplate/config/jest.config' );
 
-// Custom snapshot resolver for the boilerplate.
-config.snapshotResolver = './snapshot-resolver.ts';
-
 config.moduleNameMapper = {
 	...config.moduleNameMapper,
 	'eslint-plugin-svelte': '<rootDir>/mocks/eslint-plugin-svelte.ts',
