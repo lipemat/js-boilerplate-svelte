e8dd05f92ccc82dc84705c7389901340c38f920a
lipemat <mat@matlipe.com>
2026-01-07 23:35:18 -0500

    Remove js-boilerplate as a dependency to run tests
    No need to install the extra 1000 packages just for testing

--- jest/jest.config.ts
+++ jest/jest.config.ts
@@ -1,14 +1,14 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
-import config from '@lipemat/js-boilerplate/config/jest.config.js';
+import jestConfig from '@lipemat/js-boilerplate-shared/config/jest.config.mjs';
 
-config.moduleNameMapper = {
-	...config.moduleNameMapper,
+jestConfig.moduleNameMapper = {
+	...jestConfig.moduleNameMapper,
 	'eslint-plugin-svelte': '<rootDir>/mocks/eslint-plugin-svelte.ts',
 	'@sveltejs/vite-plugin-svelte': '<rootDir>/mocks/sveltejs__vite-plugin-svelte.ts',
+
 
 	// A temporary workaround for the shared package being a symlink.
 	// @todo remove once the shared package is published.
 	'^@lipemat/js-boilerplate-shared$': '<rootDir>/../../js-boilerplate-shared/index.ts',
 };
 
-export default config;
+export default jestConfig;
